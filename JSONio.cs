84729ff4d5bfc9b19e0650695d9ce9d6227ce86b
Steven Carmichael <blekenbleu@gmail.com>
2024-02-17 17:50:42 -0500

    JSONio games structure

---
+++ JSONio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace JSONio
+{
+	public class Property
+	{ public string Name { get; set; } public string Value { get; set; } }
+
+	public class Car
+	{
+		public string id { get; set; }
+		public string name { get; set; }
+		public List<Property> properties { get; set; }
+	}
+	public class Game
+	{
+		public string name { get; set; }
+		public List<Car> cars { get; set; }
+	}
+	public class Games
+	{
+		public Games()
+		{
+			public List<Game> list { get; set; }
+		}
+	}
+}
