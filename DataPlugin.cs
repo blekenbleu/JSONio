e30bfd2861ae77f67e886d819ca8574ca03b69cc
Steven Carmichael <blekenbleu@gmail.com>
2024-02-19 13:51:43 -0500

    writing (null) games JSONio.json, detecting car changes
    appends new properties from JSONio.ini
    current JSONio properties are available in SimHub

--- DataPlugin.cs
+++ DataPlugin.cs
@@ -1,107 +1,192 @@
 ﻿using GameReaderCommon;
 using SimHub.Plugins;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
 using System.Windows.Media;
 
 namespace JSONio
 {
-    [PluginDescription("Device Extension Demo")]
-    [PluginAuthor("Author")]
-    [PluginName("JSONio plugin")]
-    public class DataPlugin : IPlugin, IDataPlugin, IWPFSettingsV2
-    {
-        public DataPluginSettings Settings;
+	[PluginDescription("Device Extension Demo")]
+	[PluginAuthor("Author")]
+	[PluginName("JSONio plugin")]
+	public class DataPlugin : IPlugin, IDataPlugin, IWPFSettingsV2
+	{
+		public DataPluginSettings Settings;
+		internal static readonly string My = "JSONio."; 
+		internal static readonly string Ini = "DataCorePlugin.ExternalScript." + My; // configuration source
+		private string path;			// JSONio.ini 'JSONio.file' property value
+		private bool changed;
+		public Games games;
+		internal List<Property> previous, current, init;
 
-        /// <summary>
-        /// Instance of the current plugin manager
-        /// </summary>
-        public PluginManager PluginManager { get; set; }
+		/// <summary>
+		/// wraps SimHub.Logging.Current.Info(); prefixes MIDIio.My
+		/// </summary>
+		internal static bool Info(string str)
+		{
+			SimHub.Logging.Current.Info(DataPlugin.My + str);   // bool Info()
+			return true;
+		}
 
-        /// <summary>
-        /// Gets the left menu icon. Icon must be 24x24 and compatible with black and white display.
-        /// </summary>
-        public ImageSource PictureIcon => this.ToIcon(Properties.Resources.sdkmenuicon);
+		/// <summary>
+		/// Instance of the current plugin manager
+		/// </summary>
+		public PluginManager PluginManager { get; set; }
 
-        /// <summary>
-        /// Gets a short plugin title to show in left menu. Return null if you want to use the title as defined in PluginName attribute.
-        /// </summary>
-        public string LeftMenuTitle => "JSONio plugin";
+		/// <summary>
+		/// Gets the left menu icon. Icon must be 24x24 and compatible with black and white display.
+		/// </summary>
+		public ImageSource PictureIcon => this.ToIcon(Properties.Resources.sdkmenuicon);
 
-        /// <summary>
-        /// Called one time per game data update, contains all normalized game data,
-        /// raw data are intentionnally "hidden" under a generic object type (A plugin SHOULD NOT USE IT)
-        ///
-        /// This method is on the critical path, it must execute as fast as possible and avoid throwing any error
-        ///
-        /// </summary>
-        /// <param name="pluginManager"></param>
-        /// <param name="data">Current game data, including current and previous data frame.</param>
-        public void DataUpdate(PluginManager pluginManager, ref GameData data)
-        {
-            // Define the value of our property (declared in init)
-            if (data.GameRunning)
-            {
-                if (data.OldData != null && data.NewData != null)
-                {
-                    if (data.OldData.SpeedKmh < 100 && data.OldData.SpeedKmh >= 100)
-                    {
-                        // Trigger an event
-                        this.TriggerEvent("SpeedWarning");
-                    }
-                }
-            }
-        }
+		/// <summary>
+		/// Gets a short plugin title to show in left menu. Return null if you want to use the title as defined in PluginName attribute.
+		/// </summary>
+		public string LeftMenuTitle => "JSONio plugin";
 
-        /// <summary>
-        /// Called at plugin manager stop, close/dispose anything needed here !
-        /// Plugins are rebuilt at game change
-        /// </summary>
-        /// <param name="pluginManager"></param>
-        public void End(PluginManager pluginManager)
-        {
-            // Save settings
-            this.SaveCommonSettings("GeneralSettings", Settings);
-        }
+		/// <summary>
+		/// Called one time per game data update, contains all normalized game data,
+		/// raw data are intentionnally "hidden" under a generic object type (A plugin SHOULD NOT USE IT)
+		///
+		/// This method is on the critical path, it must execute as fast as possible and avoid throwing any error
+		///
+		/// </summary>
+		/// <param name="pluginManager"></param>
+		/// <param name="data">Current game data, including current and previous data frame.</param>
+		public void DataUpdate(PluginManager pluginManager, ref GameData data)
+		{
+/*
+			// Define the value of our property (declared in init)
+			if (data.GameRunning)
+			{
+				if (data.OldData != null && data.NewData != null)
+				{
+					if (data.OldData.SpeedKmh < 100 && data.OldData.SpeedKmh >= 100)
+					{
+						// Trigger an event
+						this.TriggerEvent("SpeedWarning");
+					}
+				}
+			}
+ */
+		}
 
-        /// <summary>
-        /// Returns the settings control, return null if no settings control is required
-        /// </summary>
-        /// <param name="pluginManager"></param>
-        /// <returns></returns>
-        public System.Windows.Controls.Control GetWPFSettingsControl(PluginManager pluginManager)
-        {
-            return new SettingsControl(this);
-        }
+		/// <summary>
+		/// Called at plugin manager stop, close/dispose anything needed here !
+		/// Plugins are rebuilt at game change
+		/// </summary>
+		/// <param name="pluginManager"></param>
+		public void End(PluginManager pluginManager)
+		{
+			// Save settings
+			Settings.properties = current;
+			this.SaveCommonSettings("GeneralSettings", Settings);
+			if (changed)
+				File.WriteAllText(path, JsonSerializer.Serialize(games, new JsonSerializerOptions { WriteIndented = true }));
+				
+		}
 
-        /// <summary>
-        /// Called once after plugins startup
-        /// Plugins are rebuilt at game change
-        /// </summary>
-        /// <param name="pluginManager"></param>
-        public void Init(PluginManager pluginManager)
-        {
-            SimHub.Logging.Current.Info("Starting plugin");
+		/// <summary>
+		/// Returns the settings control, return null if no settings control is required
+		/// </summary>
+		/// <param name="pluginManager"></param>
+		/// <returns></returns>
+		public System.Windows.Controls.Control GetWPFSettingsControl(PluginManager pluginManager)
+		{
+			return new SettingsControl(this);
+		}
 
-            // Load settings
-            Settings = this.ReadCommonSettings<DataPluginSettings>("GeneralSettings", () => new DataPluginSettings());
+		/// <summary>
+		/// Called once after plugins startup
+		/// Plugins are rebuilt at game change
+		/// </summary>
+		/// <param name="pluginManager"></param>
+		public void Init(PluginManager pluginManager)
+		{
+			SimHub.Logging.Current.Info("JSONio Init()");
+			changed = false;	// write JSON file during End() only if true
 
-            // Declare a property available in the property list, this gets evaluated "on demand" (when shown or used in formulas)
-            this.AttachDelegate("CurrentDateTime", () => DateTime.Now);
+			// Load properties from settings
+			Settings = this.ReadCommonSettings<DataPluginSettings>("GeneralSettings", () => new DataPluginSettings());
+			previous = Settings.properties;
+			init = new List<Property>();
 
-            // Declare an event
-            this.AddEvent("SpeedWarning");
+			// Load properties from JSONio.ini
+			string pts, ds = pluginManager.GetPropertyValue(pts = Ini + "properties")?.ToString();
+			List<string> props;
+			if (!(null == ds && Info($"Init(): '{pts}' not found")))
+			{
+//				string report = $"'{pts}'\n";
+				props = new List<string>(ds.Split(','));
+				foreach (string pname in props) {
+					string prop = pluginManager.GetPropertyValue(pts = Ini + pname)?.ToString();
+					if (!(null == prop && Info($"Init(): '{pts}' not found")))
+					{
+						init.Add ( new Property() { Name=pname, Value=prop });
+//						report += $"\tName={pname}, Value={prop}\n";
+					}
+				}
+//				Info("Init(): "+report);
 
-            // Declare an action which can be called
-            this.AddAction("IncrementSpeedWarning",(a, b) =>
-            {
-                SimHub.Logging.Current.Info("Speed warning incremented");
-            });
+				// append any new properties to previous
+				foreach (Property p in init)
+				{
+					int Index = previous.FindIndex(i => i.Name == p.Name);
 
-            // Declare an action which can be called
-            this.AddAction("DecrementSpeedWarning", (a, b) =>
-            {
-                SimHub.Logging.Current.Info("Speed warning decremented");
-            });
-        }
-    }
+					if (-1 != Index)
+						continue;
+
+					Info("Init(): new property " + p.Name);
+					previous.Add(p);
+					if (null != games && null != games.list)
+						foreach (Game g in games.list)
+							if (g.append(p.Name, p.Value))
+								changed = true;
+				} 
+			}
+
+			current = previous;
+
+			// Declare a property available in the property list, this gets evaluated "on demand" (when shown or used in formulas)
+			foreach(Property p in current)
+				this.AttachDelegate(My+p.Name, () => p.Value);
+
+			// Declare an event
+			//this.AddEvent("SpeedWarning");
+
+			// Declare actions which can be called
+			this.AddAction("ChangeProperties",(a, b) =>
+			{
+				SimHub.Logging.Current.Info("New Car: " + pluginManager.GetPropertyValue("CarID")?.ToString());
+			});
+
+			this.AddAction("IncrementCurrentProperty", (a, b) =>
+			{
+				SimHub.Logging.Current.Info("property incremented");
+			});
+
+			this.AddAction("DecrementCurrentProperty", (a, b) =>
+			{
+				SimHub.Logging.Current.Info("property decremented");
+			});
+
+			this.AddAction("NextProperty", (a, b) =>
+			{
+				SimHub.Logging.Current.Info("current property = ");
+			});
+
+			this.AddAction("PreviousProperty", (a, b) =>
+			{
+				SimHub.Logging.Current.Info("current property = ");
+			});
+
+			// Load existing JSON
+			path = pluginManager.GetPropertyValue(Ini + "file")?.ToString();
+			if (File.Exists(path))
+			{
+				games = JsonSerializer.Deserialize<Games>(File.ReadAllText(path));
+			} else changed = true;
+		}
+	}
 }
--- README.md
+++ README.md
@@ -8,55 +8,64 @@
 		to preserve new `ProjectGuid`, etc
 	- **forgot to** update namespace from `User.PluginSdk` to `JSONio` e.g. in `Properties/`...!
 ## What
-Want properties specific to each SimHub car
+Want properties specific to each SimHub car:  
+![](Documentation/properties.png)
 - a C# list of games
 	- each game a name, a car with default properties and list of cars
 		- each car a carID and list of properties
 			- each property a name and value
+- `this.AddAction("ChangeProperties",(a, b) =>` saves current properties, if changed,  
+	then loads properties for the new Car.
+	- creates new Car object from game default if no CarID match
+	- *could* implement `this.AddEvent("CarChange");`  
+		in `public void Init(PluginManager pluginManager)`,  
+		- then `this.TriggerEvent("CarChange");`  
+			in `public void DataUpdate(PluginManager pluginManager, ref GameData data)`
+	- *but instead* let SimHub do it, by `JSONio.ini`:
+		```
+		[ExportEvent]
+		name='CarChange'
+		trigger=changed(20, [DataCorePlugin.GameData.CarId]) 
+		```
+		- *my experience*:&nbsp; SimHub ignored this **Source** when `JSONio.ini` was first loaded...  
+![](Documentation/mapping.png)  
 
-My understanding of C# is that this could be a jagged array,  
-but [List<>](https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1) better supports adding and deleting elements.
+- in `Init()`, create a new Game object in games if none exists for current Game
+	- set game from games
+
+My understanding of C# is that `games` could be a jagged array,  
+but jagged [List<>](https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1) better supports
+e.g. [adding and deleting elements](https://csharp-station.com/c-arrays-vs-lists/).
 
 ## New to me
 - C# `List<>` and particularly with non-trivial objects.
 	- [Here are some snippits](https://www.tutorialsteacher.com/csharp/csharp-list) with `List<Student>`
 	- [stackoverflow list search by LINQ](https://stackoverflow.com/questions/1175645/find-an-item-in-a-list-by-linq)
-	- [M$ Learn List<T>.FindIndex Method](https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1.findindex):&nbsp; `int index = myList.FindIndex(a => a.Prop == oProp);`  
+	- [M$ Learn List<T>.FindIndex Method](https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1.findindex):&nbsp;
 		[*index of item in a list*](https://stackoverflow.com/questions/17995706/how-can-i-get-the-index-of-an-item-in-a-list-in-a-single-step):  
 		```
-		var pair = myList.Select((Value, Index) => new { Value, Index }).Single(p => p.Value.Prop == oProp);
-		Console.WriteLine("Index:{0}; Value: {1}", pair.Index, pair.Value);
+			int index = properties.FindIndex(a => a.Name == name);
+
+            if (-1 == index)
+                properties.Add ( new Property() { Name=name, Value=value });
+            else if (replace && properties[index].Value != value)
+                properties[index].Value = value;
 		```
 - C# JSON
+	- Visual Studio had to add `System.Text.Json` package...  
 	- [pretty-print JSON from C#](https://learn.microsoft.com/en-us/dotnet/api/system.text.json.jsonserializeroptions.writeindented)  AKA
 		[serialize](https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/how-to)  
 		```
 			using System.Text.Json;
 
-			var jsonString = JsonSerializer.Serialize(yourObj, new JsonSerializerOptions { WriteIndented = true });
+			if (changed)
+                File.WriteAllText(path, JsonSerializer.Serialize(games, new JsonSerializerOptions { WriteIndented = true }));
 		```
 	- Eventually, [Read and Parse a JSON File in C#](https://code-maze.com/csharp-read-and-process-json-file/) AKA
 	 [deserialize](https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/deserialization)  
-		- [**using .NET Core**](https://stackoverflow.com/questions/13297563/read-and-parse-a-json-file-in-c-sharp)
-			[JsonSerializer Class](https://learn.microsoft.com/en-us/dotnet/api/system.text.json.jsonserializer):  
-			```
-    		public class Item
-    		{
-        		public int millis;
-        		public string stamp;
-        		public DateTime datetime;
-        		public string light;
-        		public float temp;
-        		public float vcc;
-    		}
-
-			Item item = JsonSerializer.Deserialize<Item>(File.ReadAllText("file.json"));
-
-			// get the values dynamically without declaring Item class
-			dynamic array = JsonConvert.DeserializeObject(json);
-    		foreach(var item in array)
-    		{
-        		Console.WriteLine("{0} {1}", item.temp, item.vcc);
-    		}
-
-			```
+		```
+			if (File.Exists(path))  
+            {  
+                games = JsonSerializer.Deserialize<Games>(File.ReadAllText(path));  
+            } else changed = true;  
+		```
