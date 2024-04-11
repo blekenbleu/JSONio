08340f3627c735b0502c63ae408a9c9adc48d354
Steven Carmichael <blekenbleu@gmail.com>
2024-04-11 07:55:49 -0400

    cosmetic tweaks

--- Documentation/release.sh
+++ Documentation/release.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
+echo cd ../../..
 cd ../../..
+echo 7z a /r/Temp/JSONio.zip JSONio.* NCalcScripts/JSONio.ini PluginsData/JSONio.json
 7z a /r/Temp/JSONio.zip JSONio.* NCalcScripts/JSONio.ini PluginsData/JSONio.json
