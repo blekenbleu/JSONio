44fdd0fb7c7c9724e46d1c544b2ef62ab1582332
Steven Carmichael <blekenbleu@gmail.com>
2025-02-12 11:01:34 -0500

    reorganize js.cs; no functional changes

--- js.cs
+++ js.cs
@@ -5,6 +5,29 @@ namespace blekenbleu.jsonio
 {
 	public partial class JSONio
 	{
+		// check whether current properties differ from JSON
+		internal void Changed()
+		{
+			if (-1 == gndx || -1 == cndx)
+				return;
+
+			// this should be unnecessary if slim.Reconcile() works..
+			if (gCount != slim.data.gList[gndx].cList[0].Vlist.Count
+			 || pCount != slim.data.gList[gndx].cList[cndx].Vlist.Count)
+				changed = true;
+			else for (int p = 0; p < gCount; p++)
+				// default change?
+				if (simValues[p].Default != slim.data.gList[gndx].cList[0].Vlist[p]
+						// current per-car change?
+			 	 || (p < pCount && simValues[p].Current != slim.data.gList[gndx].cList[cndx].Vlist[p]))
+				{
+					changed = true;
+					break;
+				}
+
+			View.Model.ChangedVisibility = changed ? Visibility.Visible : Visibility.Hidden;
+		}
+
 		internal void SetSlider()
 		{
 			if (0 > slider)
@@ -29,123 +52,6 @@ namespace blekenbleu.jsonio
 				Slider_factor[0] = 1;	// slider to value
 				Slider_factor[1] = 1;	// value to slider
 			}
-		}
-
-		internal string FromSlider(double value)
-		{
-			simValues[slider].Current = (Slider_factor[0] * (int)value).ToString();
-			return simValues[slider].Name + ":  " + simValues[slider].Current;
-		}
-
-		internal double ToSlider()
-		{
-			if(0 > slider)
-				return 0;
-			View.TBL.Text = simValues[slider].Name + ":  " + simValues[slider].Current;
-			return Slider_factor[1] * System.Convert.ToDouble(simValues[slider].Current);
-		}
-
-		// check whether current properties differ from JSON
-		internal void Changed()
-		{
-			if (-1 == gndx || -1 == cndx)
-				return;
-
-			// this should be unnecessary if slim.Reconcile() works..
-			if (gCount != slim.data.gList[gndx].cList[0].Vlist.Count
-			 || pCount != slim.data.gList[gndx].cList[cndx].Vlist.Count)
-				changed = true;
-			else for (int p = 0; p < gCount; p++)
-				// default change?
-				if (simValues[p].Default != slim.data.gList[gndx].cList[0].Vlist[p]
-						// current per-car change?
-			 	 || (p < pCount && simValues[p].Current != slim.data.gList[gndx].cList[cndx].Vlist[p]))
-				{
-					changed = true;
-					break;
-				}
-
-			View.Model.ChangedVisibility = changed ? Visibility.Visible : Visibility.Hidden;
-		}
-
-		/// <summary>
-		/// Helper functions used in Init() AddAction()s and Control.xaml.cs button Clicks
-		/// </summary>
-		/// <param name="sign"></param> should be 1 or -1
-		/// <param name="prefix"></param> should be "in" or "de"
-		public void Ment(int sign)
-		{
-			if (0 == Gname.Length || 0 == CurrentCar.Length)
-				return;
-			int step = Steps[View.Selection];
-			int iv = (int)(0.004 + 100 * float.Parse(simValues[View.Selection].Current));
-
-			iv += sign * step;
-			if (0 <= iv)
-			{
-				if (0 != step % 100)
-					simValues[View.Selection].Current = $"{(float)(0.01 * iv)}";
-				else simValues[View.Selection].Current = $"{(int)(0.004 + 0.01 * iv)}";
-				Changed();
-				if (slider == View.Selection)
-					View.Slslider_Point();
-			}
-		}
-
-		private void SelectedStatus()
-		{
-			if (null == View)
-				return;
-			View.Model.Selected_Property = simValues[View.Selection].Name;
-			View.Model.StatusText = Gname + " " + CurrentCar + ":\t" + View.Model.Selected_Property;
-		}
-
-		/// <summary>
-		/// Select next or prior property; exception if invoked on other than UI thread
-		/// </summary>
-		/// <param name="next"></param> false for prior
-		public void Select(bool next)
-		{
-			if (0 == Gname.Length || 0 == CurrentCar.Length)
-				return;
-
-			if (next)
-			{
-				if (++View.Selection >= simValues.Count)
-					View.Selection = 0;
-			}
-			else if (0 < View.Selection)	// prior
-				View.Selection--;
-			else View.Selection = (byte)(simValues.Count - 1);
-			SelectedStatus();
-		}
-
-		public void Swap()
-		{
-			string temp;
-			for (int i = 0; i < simValues.Count; i++)
-			{
-				temp = simValues[i].Previous;
-				simValues[i].Previous = simValues[i].Current;
-				simValues[i].Current = temp;
-			}
-			Changed();
-		}
-
-		// set "CurrentAsDefaults" action
-		internal void SetDefault()	// List<GameList> Glist)
-		{
-			if (0 == Gname.Length)
-			{
-				OOps("SetDefault: no Gname");
-				return;
-			}
-			int p = View.Selection;
-			string Current = simValues[p].Current;
-			if (0 > gndx)
-				Save_Car();
-			simValues[p].Default = Current;
-			Changed();
 		}
 
 		List<string> DefaultCopy()		// called in Save_Car()
@@ -195,12 +101,12 @@ namespace blekenbleu.jsonio
 		int GameIndex(string gnew)
 		{
 			if (1 > gnew.Length)
-				return gndx;
+				return gndx;										// should be unlikely
 
 			for (int g = 0; g < slim.data.gList.Count; g++)
 				if (0 == slim.data.gList[g].cList.Count
 				 || null == slim.data.gList[g].cList[0].Name)
-					slim.data.gList.RemoveAt(g--);
+					slim.data.gList.RemoveAt(g--);					// Reconcile() failure
 				else if (gnew == slim.data.gList[g].cList[0].Name)
 					gndx = g;
 			return gndx;
@@ -265,5 +171,100 @@ namespace blekenbleu.jsonio
 			SelectedStatus();
 			View.Model.ButtonVisibility = System.Windows.Visibility.Visible;	// ready
 		}	// CarChange()
+
+		// Control.xaml methods -------------------------------------------------
+		internal string FromSlider(double value)
+		{
+			simValues[slider].Current = (Slider_factor[0] * (int)value).ToString();
+			return simValues[slider].Name + ":  " + simValues[slider].Current;
+		}
+
+		internal double ToSlider()
+		{
+			if(0 > slider)
+				return 0;
+			View.TBL.Text = simValues[slider].Name + ":  " + simValues[slider].Current;
+			return Slider_factor[1] * System.Convert.ToDouble(simValues[slider].Current);
+		}
+
+		/// <summary>
+		/// Helper functions used in Init() AddAction()s and Control.xaml.cs button Clicks
+		/// </summary>
+		/// <param name="sign"></param> should be 1 or -1
+		/// <param name="prefix"></param> should be "in" or "de"
+		public void Ment(int sign)
+		{
+			if (0 == Gname.Length || 0 == CurrentCar.Length)
+				return;
+			int step = Steps[View.Selection];
+			int iv = (int)(0.004 + 100 * float.Parse(simValues[View.Selection].Current));
+
+			iv += sign * step;
+			if (0 <= iv)
+			{
+				if (0 != step % 100)
+					simValues[View.Selection].Current = $"{(float)(0.01 * iv)}";
+				else simValues[View.Selection].Current = $"{(int)(0.004 + 0.01 * iv)}";
+				Changed();
+				if (slider == View.Selection)
+					View.Slslider_Point();
+			}
+		}
+
+		private void SelectedStatus()		// also in CarChange()
+		{
+			if (null == View)
+				return;
+			View.Model.Selected_Property = simValues[View.Selection].Name;
+			View.Model.StatusText = Gname + " " + CurrentCar + ":\t" + View.Model.Selected_Property;
+		}
+
+		/// <summary>
+		/// Select next or prior property; exception if invoked on other than UI thread
+		/// </summary>
+		/// <param name="next"></param> false for prior
+		public void Select(bool next)
+		{
+			if (0 == Gname.Length || 0 == CurrentCar.Length)
+				return;
+
+			if (next)
+			{
+				if (++View.Selection >= simValues.Count)
+					View.Selection = 0;
+			}
+			else if (0 < View.Selection)	// prior
+				View.Selection--;
+			else View.Selection = (byte)(simValues.Count - 1);
+			SelectedStatus();
+		}
+
+		public void Swap()
+		{
+			string temp;
+			for (int i = 0; i < simValues.Count; i++)
+			{
+				temp = simValues[i].Previous;
+				simValues[i].Previous = simValues[i].Current;
+				simValues[i].Current = temp;
+			}
+			Changed();
+		}
+
+		// set "CurrentAsDefaults" action
+		internal void SetDefault()	// List<GameList> Glist)
+		{
+			if (0 == Gname.Length)
+			{
+				OOps("SetDefault: no Gname");
+				return;
+			}
+			int p = View.Selection;
+			string Current = simValues[p].Current;
+			if (0 > gndx)
+				Save_Car();
+			simValues[p].Default = Current;
+			Changed();
+		}
 	}		// public partial class JSONio
 }
