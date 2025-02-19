e6feb3bb2bee41f09395b647dba0b3de6e8996aa
Steven Carmichael <blekenbleu@gmail.com>
2025-02-19 04:30:46 -0500

    Wipe out obsolete JSON content for .ini changes only if game play

--- Control.xaml.cs
+++ Control.xaml.cs
@@ -17,7 +17,7 @@ namespace blekenbleu.jsonio
 		public JSONio JS { get; }
 		public ViewModel Model;							// reference XAML controls
 		internal byte Selection;						// changes only in JSONio.Select() on UI thread
-		internal static string version = "1.53";
+		internal static string version = "1.54";
 
 		public Control() {								// called before simValues are initialized
 			Model = new ViewModel(this);
